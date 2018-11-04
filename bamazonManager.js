e0d5909df82918af039ad67af3555c0346528f48
Short Stack <41342213+rchlblns@users.noreply.github.com>
2018-11-03 21:30:51 -0500

    prompts added

--- bamazonManager.js
+++ bamazonManager.js
@@ -17,6 +17,22 @@ connection.connect(function (err) {
     //if error, display error
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    //otherwise display items for sale
+    //otherwise shows actions for Manager to complete
     selectAction();
 });
+
+function selectAction(){
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "action",
+            message: "Hello manager! What would you like to do?",
+            choices: [
+                "View Products for Sale",
+                "View Low Inventory",
+                "Add to Inventory",
+                "Add New Product"
+            ]
+        }
+    ])
+}
